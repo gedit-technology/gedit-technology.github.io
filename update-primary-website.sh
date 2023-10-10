78d8f4d069acabf5a821fdbca4df6db6d520270d
Sébastien Wilmet <sebastien.wilmet@gmail.com>
2023-10-10 16:00:00 +0200

    Implement update-primary-website.sh

---
+++ update-primary-website.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+src='../gedit-technology-website/src/'
+dest='docs/'
+
+./check-directory-content.sh "${src}" || exit 1
+
+# --archive equals -rlptgoD (keep only what's necessary).
+rsync -rlp --delete --force --progress --stats	\
+	--filter '- /user-manuals/'		\
+	"${src}" "${dest}"
